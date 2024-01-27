e6cd092c3d1c6f27ef04e066b4c2d0e1df6bf725
McQueen <m.gorelenko@mail.ru>
2024-01-27 20:57:43 +0300

    feat: add cd operation

---
+++ app/operations/cd.js
@@ -0,0 +1,14 @@
+import { workingDirectory } from "../utils/helpers.js";
+
+// only works with an absolute path
+
+const cd = async (path) => {
+    try {
+      process.chdir(path)
+      workingDirectory();
+    } catch (error) {
+      console.error ('Operation failed')
+    }
+};
+
+export { cd }
--- app/utils/handler.js
+++ app/utils/handler.js
@@ -1,4 +1,5 @@
 import { rl } from '../../index.js';
+import { cd } from '../operations/cd.js';
 import { ls  } from '../operations/ls.js';
 import { up } from '../operations/up.js';
 
@@ -18,6 +19,15 @@ function handler(command) {
           break;
         case '.exit':
           rl.close();
+          break;
+        default:
+          throw new Error('Invalid input');
+      }
+    } else if (operation.length === 2) {
+      let args = operation[1].toString();
+      switch(operation[0].toString()) {
+        case 'cd':
+          cd(args);
           break;
         default:
           throw new Error('Invalid input');
